e97888146eaada9dbac649d305e1b9abc53af215
LukaszSyrewicz <157284773+LukaszSyrewicz@users.noreply.github.com>
2025-01-15 13:02:43 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.txtPriority = new System.Windows.Forms.TextBox();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.btnDeleteTask = new System.Windows.Forms.Button();
+            this.btnSortTasks = new System.Windows.Forms.Button();
+            this.dgvTasks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).BeginInit();
+            this.SuspendLayout();
+
+            // txtTitle
+            this.txtTitle.Location = new System.Drawing.Point(12, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 23);
+            this.txtTitle.Text = "Tytuł zadania";
+            this.txtTitle.ForeColor = System.Drawing.Color.Gray;
+            this.txtTitle.Enter += new System.EventHandler(this.TextBox_Enter);
+            this.txtTitle.Leave += new System.EventHandler(this.TextBox_Leave);
+
+            // txtCategory
+            this.txtCategory.Location = new System.Drawing.Point(12, 41);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(200, 23);
+            this.txtCategory.Text = "Kategoria";
+            this.txtCategory.ForeColor = System.Drawing.Color.Gray;
+            this.txtCategory.Enter += new System.EventHandler(this.TextBox_Enter);
+            this.txtCategory.Leave += new System.EventHandler(this.TextBox_Leave);
+
+            // txtPriority
+            this.txtPriority.Location = new System.Drawing.Point(12, 70);
+            this.txtPriority.Name = "txtPriority";
+            this.txtPriority.Size = new System.Drawing.Size(200, 23);
+            this.txtPriority.Text = "Priorytet (liczba)";
+            this.txtPriority.ForeColor = System.Drawing.Color.Gray;
+            this.txtPriority.Enter += new System.EventHandler(this.TextBox_Enter);
+            this.txtPriority.Leave += new System.EventHandler(this.TextBox_Leave);
+
+            // dtpDueDate
+            this.dtpDueDate.Location = new System.Drawing.Point(12, 99);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(200, 23);
+
+            // btnAddTask
+            this.btnAddTask.Location = new System.Drawing.Point(12, 128);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(200, 30);
+            this.btnAddTask.Text = "Dodaj zadanie";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+
+            // btnDeleteTask
+            this.btnDeleteTask.Location = new System.Drawing.Point(12, 164);
+            this.btnDeleteTask.Name = "btnDeleteTask";
+            this.btnDeleteTask.Size = new System.Drawing.Size(200, 30);
+            this.btnDeleteTask.Text = "Usuń zaznaczone";
+            this.btnDeleteTask.UseVisualStyleBackColor = true;
+            this.btnDeleteTask.Click += new System.EventHandler(this.btnDeleteTask_Click);
+
+            // btnSortTasks
+            this.btnSortTasks.Location = new System.Drawing.Point(12, 200);
+            this.btnSortTasks.Name = "btnSortTasks";
+            this.btnSortTasks.Size = new System.Drawing.Size(200, 30);
+            this.btnSortTasks.Text = "Sortuj zadania";
+            this.btnSortTasks.UseVisualStyleBackColor = true;
+            this.btnSortTasks.Click += new System.EventHandler(this.btnSortTasks_Click);
+
+            // dgvTasks
+            this.dgvTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTasks.Location = new System.Drawing.Point(230, 12);
+            this.dgvTasks.Name = "dgvTasks";
+            this.dgvTasks.Size = new System.Drawing.Size(500, 300);
+            this.dgvTasks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 330);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtPriority);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.btnDeleteTask);
+            this.Controls.Add(this.btnSortTasks);
+            this.Controls.Add(this.dgvTasks);
+            this.Name = "Form1";
+            this.Text = "Task Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.TextBox txtPriority;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Button btnDeleteTask;
+        private System.Windows.Forms.Button btnSortTasks;
+        private System.Windows.Forms.DataGridView dgvTasks;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        // Lista do przechowywania zadań
+        private List<TaskItem> tasks = new List<TaskItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            // Pobierz dane z pól
+            string title = txtTitle.Text;
+            string category = txtCategory.Text;
+            int priority;
+            DateTime dueDate = dtpDueDate.Value;
+
+            // Walidacja danych
+            if (string.IsNullOrWhiteSpace(title) || !int.TryParse(txtPriority.Text, out priority))
+            {
+                MessageBox.Show("Proszę podać poprawne dane dla zadania.");
+                return;
+            }
+
+            // Dodaj nowe zadanie do listy
+            tasks.Add(new TaskItem
+            {
+                Title = title,
+                Category = category,
+                Priority = priority,
+                DueDate = dueDate
+            });
+
+            // Zaktualizuj widok
+            UpdateTaskGrid();
+            ClearInputFields();
+        }
+
+        private void btnDeleteTask_Click(object sender, EventArgs e)
+        {
+            if (dgvTasks.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgvTasks.SelectedRows)
+                {
+                    TaskItem task = row.DataBoundItem as TaskItem;
+                    tasks.Remove(task);
+                }
+                UpdateTaskGrid();
+            }
+            else
+            {
+                MessageBox.Show("Proszę zaznaczyć zadanie do usunięcia.");
+            }
+        }
+
+        private void btnSortTasks_Click(object sender, EventArgs e)
+        {
+            tasks = tasks.OrderBy(t => t.Priority).ThenBy(t => t.DueDate).ToList();
+            UpdateTaskGrid();
+        }
+
+        private void UpdateTaskGrid()
+        {
+            dgvTasks.DataSource = null;
+            dgvTasks.DataSource = tasks;
+        }
+
+        private void ClearInputFields()
+        {
+            txtTitle.Clear();
+            txtCategory.Clear();
+            txtPriority.Clear();
+            dtpDueDate.Value = DateTime.Now;
+        }
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null && textBox.ForeColor == System.Drawing.Color.Gray)
+            {
+                textBox.Text = string.Empty;
+                textBox.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void TextBox_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                if (textBox == txtTitle)
+                {
+                    textBox.Text = "Tytuł zadania";
+                }
+                else if (textBox == txtCategory)
+                {
+                    textBox.Text = "Kategoria";
+                }
+                else if (textBox == txtPriority)
+                {
+                    textBox.Text = "Priorytet (liczba)";
+                }
+
+                textBox.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+    }
+
+    // Klasa reprezentująca zadanie
+    public class TaskItem
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Priority { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
